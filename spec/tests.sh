8f9e0d2de62fd429c751430e186fbfd5af8eecb4
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-07-08 14:45:08 +0800

    ci: Run tests

--- spec/tests.sh
+++ spec/tests.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
 
 test_existence_of_auth_code sfs
 test_existence_of_auth_name 'Office of the Royal Society'
 test_existence_of_auth_remark 'Formerly named The Royal Institute of Thailand'
+for file in {cc,iso}-24229.{html,pdf,doc}
+do
+  test_existence_of_file "${file}"
+done
