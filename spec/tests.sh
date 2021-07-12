c2baa1eb50561c4b7b94924fdf27af0e484308eb
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-07-12 13:51:18 +0800

    ci: Add more tests

--- spec/tests.sh
+++ spec/tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+for file in index.html documents.xml
+do
+  outdir=site test_existence_of_file "${file}"
+done
+
 test_existence_of_auth_code sfs
 test_existence_of_auth_name 'Office of the Royal Society'
 test_existence_of_auth_remark 'Formerly named The Royal Institute of Thailand'
