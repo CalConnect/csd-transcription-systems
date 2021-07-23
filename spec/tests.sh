b5c3f5dc48c8d9775b7b5505f68a912c6176da1c
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-07-23 12:05:23 +0800

    test: Add tests for PDF output existence

--- spec/tests.sh
+++ spec/tests.sh
@@ -5,10 +5,14 @@ do
   outdir=site test_existence_of_file "${file}"
 done
 
-test_existence_of_auth_code sfs
-test_existence_of_auth_name 'Office of the Royal Society'
-test_existence_of_auth_remark 'Formerly named The Royal Institute of Thailand'
 for file in {cc,iso}-24229.{html,pdf,doc}
 do
   test_existence_of_file "${file}"
 done
+
+test_file_type iso-24229.pdf PDF
+test_file_type cc-24229.pdf PDF
+
+test_existence_of_auth_code sfs
+test_existence_of_auth_name 'Office of the Royal Society'
+test_existence_of_auth_remark 'Formerly named The Royal Institute of Thailand'
