51a791c35371fdefdf65153ed77635393487e3b2
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-07-27 18:32:52 +0800

    ci: Update tests

--- spec/tests.sh
+++ spec/tests.sh
@@ -13,6 +13,9 @@ done
 test_file_type iso-24229.pdf PDF
 test_file_type cc-24229.pdf PDF
 
-test_existence_of_auth_code sfs
-test_existence_of_auth_name 'Office of the Royal Society'
-test_existence_of_auth_remark 'Formerly named The Royal Institute of Thailand'
+test_existence_of_auth_id ALA-LC
+test_exstence_of_auth_id BGN-PCGN
+test_existence_of_auth_name 'American Library Association'
+test_existence_of_auth_name 'United Nations'
+test_existence_of !bgnpcgn
+test_existence_of !alalc
