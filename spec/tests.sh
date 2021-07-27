4bbda34fbe4c59d55098d6fb5de874c257e906f2
Jeffrey Lau <jeffrey.lau@ribose.com>
2021-07-27 19:22:40 +0800

    ci: Fix tests

--- spec/tests.sh
+++ spec/tests.sh
@@ -14,8 +14,8 @@ test_file_type iso-24229.pdf PDF
 test_file_type cc-24229.pdf PDF
 
 test_existence_of_auth_id ALA-LC
-test_exstence_of_auth_id BGN-PCGN
+test_existence_of_auth_id BGN-PCGN
 test_existence_of_auth_name 'American Library Association'
 test_existence_of_auth_name 'United Nations'
-test_existence_of !bgnpcgn
 test_existence_of !alalc
+test_existence_of !bgnpcgn
